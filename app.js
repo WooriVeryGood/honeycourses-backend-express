996a5a2e8496d679ac2d69831d1068ce2bfcfeb2
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2023-05-14 01:21:08 +0800

    API 외부 접속 차단 시도

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const logger = require('morgan');
 const coursesRouter = require('./routes/courses');
 const cors = require("cors");
 
-
 const app = express();
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors({
+  origin: 'https://honeycourses.com'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', coursesRouter); // add your API routes here
+app.use('/api', coursesRouter);
 
+// Add error handling middleware to catch unauthorized requests
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+});
 
 module.exports = app;
