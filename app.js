1a7b9bbcbc06853bfee406fa79bc3f92ae0ffca6
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2023-05-14 01:57:19 +0800

    IP주소 반영

--- app.js
+++ app.js
@@ -18,17 +18,19 @@ const corsOptions = {
 
 const morganMiddleware = logger(function (tokens, req, res) {
   return [
-      chalk.hex('#34ace0').bold(tokens.method(req, res)),
-      chalk.hex('#ffb142').bold(tokens.status(req, res)),
-      chalk.hex('#ff5252').bold(tokens.url(req, res)),
-      chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
-      chalk.hex('#f78fb3').bold('@ ' + dayjs().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')),
-      chalk.yellow(tokens['remote-addr'](req, res)),
-      chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
-      chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
-      '\n',
+    chalk.hex('#34ace0').bold(tokens.method(req, res)),
+    chalk.hex('#ffb142').bold(tokens.status(req, res)),
+    chalk.hex('#ff5252').bold(tokens.url(req, res)),
+    chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
+    chalk.hex('#f78fb3').bold('@ ' + dayjs().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')),
+    chalk.yellow(req.ip),
+    chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
+    chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
+    '\n',
   ].join(' ');
 });
+
+app.set('trust proxy', true);
 
 app.use(morganMiddleware);
 
