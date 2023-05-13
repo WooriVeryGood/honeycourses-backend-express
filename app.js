30dfefd13285a43e8d765506f900a873a8564745
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2023-05-14 01:51:25 +0800

    로그양식 수정, 403 에러 추가

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const chalk = require('chalk')
 const express = require('express');
 const logger = require('morgan');
 const coursesRouter = require('./routes/courses');
 const cors = require("cors");
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 
 const app = express();
 
@@ -9,11 +16,31 @@ const corsOptions = {
   origin: 'https://honeycourses.com'
 };
 
-app.use(logger('dev'));
+const morganMiddleware = logger(function (tokens, req, res) {
+  return [
+      chalk.hex('#34ace0').bold(tokens.method(req, res)),
+      chalk.hex('#ffb142').bold(tokens.status(req, res)),
+      chalk.hex('#ff5252').bold(tokens.url(req, res)),
+      chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
+      chalk.hex('#f78fb3').bold('@ ' + dayjs().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')),
+      chalk.yellow(tokens['remote-addr'](req, res)),
+      chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
+      chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
+      '\n',
+  ].join(' ');
+});
+
+app.use(morganMiddleware);
+
+// app.use(logger('dev'));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', coursesRouter); // add your API routes here
+
+app.use('/api/*', function(req, res) {
+  res.status(403).send('Access forbidden.');
+});
 
 module.exports = app;
